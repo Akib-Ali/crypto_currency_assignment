19d250d429768e8041c8cf3804f3060dcbb06015
Akib Ali <akib7599@gmail.com>
2022-11-25 19:08:41 +0530

    add header

---
+++ src/container/header.jsx
@@ -0,0 +1,55 @@
+import Styles from "./header.module.css"
+import { Box ,Flex,Text} from "@chakra-ui/react"
+export const Header = () => {
+
+    var data = [
+        {
+            title: "MARKETCAP",
+            value: "$1.15T"
+        },
+        {
+            title: "EXCHANGE VOL",
+            value: "$50.69"
+        },
+        {
+            title: "ASSESTS",
+            value: "2,295"
+        }, {
+            title: "EXCHANGES",
+            value: "73"
+        }, {
+            title: "MARKETS",
+            value: "14,054"
+        },
+        {
+            title: "BTC DOM INDEX",
+            value: "31.9%"
+        },
+
+    ]
+    return (
+        // <div className={Styles.container}>
+
+        //     {data.map((elem,index) => (
+        //         <div className={Styles.data} key={index}>
+
+        //             <Box>{elem.title}</Box>
+        //             <Box>{elem.value}</Box>
+        //         </div>
+
+        //     ))}
+        // </div>
+        <Box bg="#184b8d">
+        <Flex    border="2px solid red" w={["100%", "90%"]} m="auto" justifyContent={"space-between"} 
+        flexWrap  ="wrap" height={"150px"} pt="15px">
+          {data.map((elem,index)=>(
+            <Box key={index}> 
+                <Text color={"white"}>{elem.title}</Text>
+                <Text color={"white"}>{elem.value}</Text>
+            </Box>
+
+          ))}
+        </Flex>
+        </Box>
+    )
+}
